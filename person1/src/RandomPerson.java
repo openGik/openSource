1cf776ed28771b4681be00369e8ab1e0de731145
nikolaydou <controlkolia@gmail.com>
2018-07-30 01:39:08 +0300

    create email

--- person1/src/RandomPerson.java
+++ person1/src/RandomPerson.java
@@ -8,9 +8,10 @@ public class RandomPerson {
             lastname[] = {" petrov" , "ivanov" , "stavruchenko" , "jorin" , "buhtin"} ,
             surname[] = {"ivanovich" , "sidorovich" , "gennadievich" , "chehovich" , "jorovich"} ,
             number[] = {" 8-907-999-98-89 ", "8-999-909-99-99" ," 8-495-495-48-48 ", "8-903-237-22-33"} ,
-            job[] = {"ingener ","mihanik "," voditel" , "partnoy"};
+            job[] = {"ingener ","mihanik "," voditel" , "partnoy" },
+    email[]={"anerodaq-5218@yopmail.com" , "tretik@yopmail.com", "8-495-495-48-48@mail.ru" , "gennadievich12365456@gmail.com"};
     private double zp[] ={90000 , 9000 , 83222 , 8888888 , 50000 , 63277 , 34000} ;
-    private int age[] ={34,56,67,79,23,35,21,89,76,54,43,32,90} ;
+    private int age[] ={34,56,67,79,23,35,21,89,76,54,43,32,90 ,40 , 40 ,40} ;
     private Random rand = new Random();
     public Person[] create(int kol){
         Person pers[] = new Person[kol];
@@ -20,6 +21,7 @@ public class RandomPerson {
                     lastname[rand.nextInt(Math.abs(lastname.length-1))]
                     , number[rand.nextInt(Math.abs(number.length-1))] ,
                     job[rand.nextInt(Math.abs(job.length-1))] ,
+                    email[rand.nextInt(Math.abs(email.length-1))] ,
                     zp[rand.nextInt(Math.abs(zp.length-1))] ,
                     age[rand.nextInt(Math.abs(age.length-1))]);
         }
