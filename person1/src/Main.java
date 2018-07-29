91ad074eb058670ce79144084dac06cdd61bf121
nikolaydou <controlkolia@gmail.com>
2018-07-30 01:29:52 +0300

    main file

--- person1/src/Main.java
+++ person1/src/Main.java
@@ -3,6 +3,11 @@
  */
 public class Main {
     public static void main(String[] args){
-        System.out.print(34);
+        Person[] pers;
+        int kol_obj = 15;
+        pers = new RandomPerson().create(kol_obj);
+        for(int i = 0 ; i < kol_obj ; i++)
+        pers[i].printPersonAgeUsl(">40");
+
     }
 }
