a9306803188d2d61542cc7d75d1ef6985ace809d
nikolaydou <40890773+nikolaydou@users.noreply.github.com>
2018-07-31 04:00:50 +0300

    master (#1)
    * obnovil class
    
    * auto zapolnenie
    
    * main file
    
    * update
    
    * update
    
    * create email
    
    * one updete
    
    * 1

--- person1/src/Main.java
+++ person1/src/Main.java
@@ -3,6 +3,11 @@
  */
 public class Main {
     public static void main(String[] args){
-        
+        Person[] pers;
+        int kol_obj = 15;
+        pers = new RandomPerson().create(kol_obj);
+        for(int i = 0 ; i < kol_obj ; i++)
+        pers[i].printPersonAgeUsl(">40");
+
     }
 }
