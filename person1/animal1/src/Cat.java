f9f76c5096fbc3dc19eab48d76822a3f0ed96738
nikolaydou <controlkolia@gmail.com>
2018-08-11 16:49:39 +0300

    fan
    lesson 7 eat class

--- person1/animal1/src/Cat.java
+++ person1/animal1/src/Cat.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 /**
  * Created by Kolya on 01.08.2018.
  * * 1. Создать классы Собака и Кот с наследованием от класса Животное.
@@ -7,9 +9,19 @@
  5. * Добавить животным разброс в ограничениях. То есть у одной собаки ограничение на бег может быть 400 м., у другой 600 м.
  */
 public class Cat extends Animal {
+    private int eat_fuul = 0;
+    private boolean flag_eat = false;
 Cat(String name , int age){
-super(name , age);
+    super(name , age);
+    eat_fuul = new Random().nextInt(20);
+    if(eat_fuul<10)
+        eat_fuul+=10;
 }
+    Cat(String name , int age , int eat){
+        super(name , age);
+        eat_fuul = eat;
+
+    }
     public void speed() {
         super.speed();
     }
@@ -27,6 +39,17 @@ super(name , age);
             super.info_animal();
         else
             System.out.print("error");
+    }
+    public void eat(Eat eat_caposite){
+        if (!eat_caposite.isFlag_no_fuul()&eat_caposite.getCaposite()>=eat_fuul) {
+            eat_caposite.setCaposite(eat_caposite.getCaposite() - eat_fuul);
+            System.out.println("кот наелся");
+        }
+        else if(eat_caposite.isFlag_no_fuul())
+            System.out.println("еды нет");
+        else
+            System.out.println("не хватило еды");
+
     }
 
 }
--- person1/animal1/src/Main_animal.java
+++ person1/animal1/src/Main_animal.java
@@ -3,11 +3,15 @@
  */
 public class Main_animal {
     public static void main(String[] args){
-    Cat cat = new Cat("barsic",13);
-        Dog dog = new Dog("puh",17);
-        dog.info_animal();
-        dog.info_animal_dog();
-        cat.info_animal();
-        cat.info_animal_cat();
+    Cat[] cat = new Cat[5];
+
+            cat[0] = new  Cat("barsik",5 , 12);
+            cat[1] = new  Cat("vasia",4 , 20);
+            cat[2] = new  Cat("gor",9 , 14);
+            cat[3] = new  Cat("fog",23 , 8);
+            cat[4] = new  Cat("nok",12 , 5);
+            Eat eat = new Eat(20);
+                for(int i = 0 ; i<cat.length ; i++)
+                    cat[i].eat(eat);
         }
 }
