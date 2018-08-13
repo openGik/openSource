4d73ae0a1299d61b19b857554418ea7cc7968918
nikolaydou <40890773+nikolaydou@users.noreply.github.com>
2018-08-13 10:56:14 +0300

    Create t.java

---
+++ untitled/src/t.java
@@ -0,0 +1,11 @@
+package rt;
+import MYdefault.MAIN;
+public class t {
+	public static void main(String[] args) {
+		 javax.swing.SwingUtilities.invokeLater(new Runnable() {
+				public void run() {
+	             new MAIN("proga 8");
+	             }
+	        });
+	   }
+	}
